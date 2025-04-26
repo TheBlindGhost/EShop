19f3decc3d67e5b2bb3ff9a1af2af1ff67bf29f9
User <User@DESKTOP-DI0GELS>
2025-04-26 20:55:58 +0200

    Add project files.

---
+++ EShop/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace EShop.Models
+{
+    public class Category : BaseModel
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+    }
+}
